a800a467006e57a3aa5792cd0a167a176883c676
renxun <renxun@doupai.cc>
2022-03-02 15:39:40 +0800

    feat: 连续绘制效果测试

--- 转场练习/OpenGL/GLSL/vertexShader2.glsl
+++ 转场练习/OpenGL/GLSL/vertexShader2.glsl
@@ -3,10 +3,15 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
+uniform mat4 scale;
+uniform mat4 rotation;
+uniform mat4 translation;
+
 out vec2 TexCoord; // 纹理坐标输出
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    vec4 position = vec4(aPos, 1.0);
+    gl_Position = rotation * scale * translation * position;
     TexCoord = aTexCoord;
 }
--- 转场练习/OpenGL/ViewController.mm
+++ 转场练习/OpenGL/ViewController.mm
@@ -13,6 +13,10 @@
 #import "UIImage+Util.h"
 #import "ImageHelper.h"
 
+#import <glm/glm.hpp>
+#import <glm/gtc/matrix_transform.hpp>
+#import <glm/gtc/type_ptr.hpp>
+
 #define kWidth [UIScreen mainScreen].bounds.size.width
 #define kHeight [UIScreen mainScreen].bounds.size.height
 
@@ -23,155 +27,147 @@
     GLuint frameBuff;
     GLuint renderBuff;
     
-    // FBO test
     GLuint textureID1;
-    GLuint frameBufferID;
-    
     GLuint textureID2;
-    GLuint renderBufferID2;
-    
     
     int width;
     int height;
-
+    
+    GLuint VAO;
 }
-@property (nonatomic, strong) CGLView *glView;
+@property (nonatomic, strong) IBOutlet CGLView *glView;
+@property (weak, nonatomic) IBOutlet UIView *operatorView;
 @property (nonatomic, assign) CGPoint lastPoint;
+@property (weak, nonatomic) IBOutlet UISlider *slder1;
+@property (weak, nonatomic) IBOutlet UISlider *slider2;
+@property (weak, nonatomic) IBOutlet UISlider *slider3;
+@property (weak, nonatomic) IBOutlet UISlider *slider4;
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self initSlider];
+
     CGFloat scale = [UIScreen mainScreen].scale;
-    width = self.view.frame.size.width * scale;
-    height = self.view.frame.size.height * scale;
-    
+    width = self.glView.frame.size.width * scale;
+    height = self.glView.frame.size.height * scale;
     [self _setContext];
     [self _buildGLViewAndBindBuffer];
     
+    // draw
+    [self drawMixImage];
+}
+
+- (void)initSlider {
+    self.slder1.minimumValue = -1.0;
+    self.slder1.value = 0;
+    self.slder1.maximumValue = 1.0;
+
+    self.slider2.minimumValue = -1.0;
+    self.slider2.value = 0;
+    self.slider2.maximumValue = 1.0;
     
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeInfoDark];
-    [self.view addSubview:btn];
-    btn.frame = CGRectMake(100, 100, 80, 60);
-    [self.view addSubview:btn];
+    self.slider3.minimumValue = -1.0;
+    self.slider3.value = 0;
+    self.slider3.maximumValue = 1.0;
+    
+    self.slider4.minimumValue = -1.0;
+    self.slider4.value = 0;
+    self.slider4.maximumValue = 1.0;
+}
+
+#pragma mark - Action
+
+- (IBAction)sliderChange1:(UISlider *)sender {
+    [self updateTranscation:^{
+        float value = 1.f + sender.value;
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // 手动设置
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1); // 手动设置
+        glm::mat4 scale;
+        glm::mat4 rotation;
+        glm::mat4 translation;
+        scale = glm::scale(scale, glm::vec3(value, value, value));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "scale"), 1, GL_FALSE, glm::value_ptr(scale));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "rotation"), 1, GL_FALSE, glm::value_ptr(rotation));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "translation"), 1, GL_FALSE, glm::value_ptr(translation));
+    }];
+}
+
+- (IBAction)slider2Change:(UISlider *)sender {
+    [self updateTranscation:^{
+        float value = 1.f + sender.value;
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // 手动设置
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1); // 手动设置
+        glm::mat4 scale;
+        glm::mat4 rotation;
+        glm::mat4 translation;
+        static float angle = 0;
+        angle += 10;
+        rotation = glm::rotate(rotation, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "scale"), 1, GL_FALSE, glm::value_ptr(scale));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "rotation"), 1, GL_FALSE, glm::value_ptr(rotation));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "translation"), 1, GL_FALSE, glm::value_ptr(translation));
+    }];
+}
+
+- (IBAction)slider3Change:(UISlider *)sender {
+    [self updateTranscation:^{
+        float value = 1.f + sender.value;
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // 手动设置
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1); // 手动设置
+        glm::mat4 scale;
+        glm::mat4 rotation;
+        glm::mat4 translation;
+        translation = glm::translate(translation, glm::vec3(value, value, 1.0));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "scale"), 1, GL_FALSE, glm::value_ptr(scale));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "rotation"), 1, GL_FALSE, glm::value_ptr(rotation));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "translation"), 1, GL_FALSE, glm::value_ptr(translation));
+    }];
+}
+
+- (IBAction)slider4Change:(UISlider *)sender {
     
 }
 
-- (IBAction)useFBOAction:(id)sender {
-    [self drawFBO1];
-    [self drawFBO2];
-    [self mixDraw];
+- (void)updateTranscation:(void(^)(void))codeBlock {
+    [self begin];
+    codeBlock();
+    [self draw];
 }
 
-- (void)mixDraw {
-    
+- (void)begin {
     // 使用renderBuffer为颜色缓冲区
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, renderBuff);
-    glBindRenderbuffer(GL_RENDERBUFFER, renderBuff);
-    glBindBuffer(GL_FRAMEBUFFER, frameBuff);
-    
     self->shaderProgram = [GLESUtil creatShaderProgramWithVertextShaderName:@"vertexShader2"
                                                          fragmentShaderName:@"fragmentShader2"];
-    glClearColor(1, 0, 0, 1.0);
+    glClearColor(1, 1, 1, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glViewport(0, 0, width, height);
     glUseProgram(shaderProgram);
-    
-    // 指定顶点数据
-    float vertices[] = {
-        // 后面2个是纹理坐标   // texture coords
-        // positions            // texture coords
-        1.0f,  0.5f, 0.0f,    1.0f, 1.0f, // top right
-        1.0f, -0.5f, 0.0f,    1.0f, 0.0f, // bottom right
-        -1.0f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -1.0f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
-    };
-    
-    // 索引缓冲
-    unsigned int indices[] = { // 注意索引从0开始!
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-    GLuint VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    
-    // 先绑定VAO
-    glBindVertexArray(VAO);
-    
-    // 复制顶点数据 到顶点缓冲区中
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    // 绑定EBO 及复制EBO数据
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    // 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+}
 
-    // 设置纹理坐标
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,  5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // 处理纹理
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureID1);
-    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // 手动设置
-
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, textureID2);
-    glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1); // 手动设置
-
-    // 给采样器设置纹理单元
+- (void)draw {
+    // 绑定VAO 数据
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
     [context presentRenderbuffer:frameBuff];
 }
 
-- (void)drawFBO1 {
-    
-    glGenTextures(1, &textureID1);
-    glBindTexture(GL_TEXTURE_2D, textureID1);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//    // 设置纹理数据
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-
-    GLuint frameBuffer1;
-    glGenFramebuffers(1, &frameBuffer1);
-    glBindBuffer(GL_FRAMEBUFFER, frameBuffer1);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureID1, 0);
-
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (status != GL_FRAMEBUFFER_COMPLETE)
-    {
-        fprintf(stderr, "GLEW Error: %s\n", "FRAME BUFFER STATUS Error!");
-        return;
-    }
-
-    self->shaderProgram = [GLESUtil creatShaderProgramWithVertextShaderName:@"vertexShader2"
-                                                         fragmentShaderName:@"fragmentShader2"];
-    glClearColor(1, 1, 1, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glViewport(0, 0, width, height);
-    glUseProgram(shaderProgram);
+- (void)drawMixImage {
+    [self begin];
     
     // 指定顶点数据
     float vertices[] = {
         // 后面2个是纹理坐标   // texture coords
         // positions            // texture coords
-        1.0f,  0.5f, 0.0f,    1.0f, 1.0f, // top right
-        1.0f, -0.5f, 0.0f,    1.0f, 0.0f, // bottom right
-        -1.0f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -1.0f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+        1.0f,  1.0f, 0.0f,    1.0f, 1.0f, // top right
+        1.0f, -1.0f, 0.0f,    1.0f, 0.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
     
     // 索引缓冲
@@ -181,6 +177,7 @@
     };
     GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
+    self->VAO = VAO;
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
     
@@ -202,114 +199,33 @@
     // 设置纹理坐标
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,  5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    // 处理纹理
+    
     NSString *path1 = [[NSBundle mainBundle] pathForResource:@"wall.jpeg" ofType:nil];
-    [GLESUtil genTexture:0 format:GL_RGBA filePath:path1];
+    textureID1 = [GLESUtil genTexture:0 format:GL_RGBA filePath:path1];
+
     
     NSString *path2 = [[NSBundle mainBundle] pathForResource:@"awesomeface.png" ofType:nil];
-    [GLESUtil genTexture:1 format:GL_RGBA filePath:path2];
+    textureID2 = [GLESUtil genTexture:1 format:GL_RGBA filePath:path2];
+
+    
 
     glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // 手动设置
     glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1); // 手动设置
-    
-    glBindVertexArray(VAO);
 
-    // 给采样器设置纹理单元
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glBindBuffer(GL_FRAMEBUFFER, 0);
-}
+    glUniform1f(glGetUniformLocation(shaderProgram, "s"), 1);
+    glm::mat4 scale = glm::mat4(1.0f);
+//    glm::mat4 rotation;
+//    glm::mat4 translation;
+    glm::scale(scale, glm::vec3(0.1, 0.1, 0.5));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "scale"), 1, GL_FALSE, glm::value_ptr(scale));
+//    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "rotation"), 1, GL_FALSE, glm::value_ptr(rotation));
+//    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "translation"), 1, GL_FALSE, glm::value_ptr(translation));
 
-- (void)drawFBO2
-{
-    glGenTextures(1, &textureID2);
-    glBindTexture(GL_TEXTURE_2D, textureID2);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    // 设置纹理数据
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-    // 取消绑定
-    glBindTexture(GL_TEXTURE_2D, 0);
-    
-    GLuint frameBuffer2;
-    glGenFramebuffers(1, &frameBuffer2);
-    glBindBuffer(GL_FRAMEBUFFER, frameBuffer2);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureID2, 0);
-    
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (status != GL_FRAMEBUFFER_COMPLETE)
-    {
-        fprintf(stderr, "GLEW Error: %s\n", "FRAME BUFFER STATUS Error!");
-        return;
-    }
-    
-    self->shaderProgram = [GLESUtil creatShaderProgramWithVertextShaderName:@"vertexShader2"
-                                                         fragmentShaderName:@"fragmentShader2"];
-    glClearColor(1, 1, 1, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glViewport(0, 0, width, height);
-    glUseProgram(shaderProgram);
-    
-    // 指定顶点数据
-    float vertices[] = {
-        // 后面2个是纹理坐标   // texture coords
-        // positions            // texture coords
-        1.0f,  0.5f, 0.0f,    1.0f, 1.0f, // top right
-        1.0f, -0.5f, 0.0f,    1.0f, 0.0f, // bottom right
-        -1.0f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -1.0f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
-    };
-    
-    // 索引缓冲
-    unsigned int indices[] = { // 注意索引从0开始!
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-    GLuint VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    
-    // 先绑定VAO
-    glBindVertexArray(VAO);
-    
-    // 复制顶点数据 到顶点缓冲区中
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    // 绑定EBO 及复制EBO数据
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    // 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // 设置纹理坐标
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,  5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // 处理纹理
-    NSString *path1 = [[NSBundle mainBundle] pathForResource:@"王路飞.jpeg" ofType:nil];
-    [GLESUtil genTexture:0 format:GL_RGB filePath:path1];
-    
-    // 给采样器设置纹理单元
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+    [self draw];
 }
 
 #pragma mark - 初始化
 - (void)_buildGLViewAndBindBuffer {
-    CGLView *glView = [CGLView new];
-    glView.backgroundColor = UIColor.lightGrayColor;
-    [self.view addSubview:glView];
-    glView.frame = CGRectMake(0, 0, kWidth, kHeight);
     // 生成渲染缓存 - 存储像素数据
     glGenRenderbuffers(1, &renderBuff);
     glBindRenderbuffer(GL_RENDERBUFFER, renderBuff);
@@ -320,7 +236,7 @@
     // 将渲染缓冲区挂载到当前帧缓冲区上
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, renderBuff);
     // 将可绘制对象的存储绑定到OpenGL ES renderbuffer对象。 传递层对象作为参数来分配其存储空间。宽度，高度和像素格式取自层，用于为renderbuffer分配存储空间
-    [context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(id<EAGLDrawable>)glView.layer];
+    [context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(id<EAGLDrawable>)self.glView.layer];
 }
 
 - (void)_setContext {
